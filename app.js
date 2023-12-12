8da38266435240b3f220ac58b402ffc54e5af1be
Ramazan Mert Kaya <kmert0@outlook.com>
2023-12-12 19:12:11 +0300

    Working with Cookies

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const retrBtn = document.getElementById('retrieve-btn');
 
 storeBtn.addEventListener('click', () => {
     const userID = 'u123'
+    const user = {
+        name: 'Mert',
+        age: 23,
+        hobbies: ['Sports', 'Music']
+    };
     document.cookie = `uid=${userID}`;
+    document.cookie = `user=${JSON.stringify(user)}`;
 });
 
 retrBtn.addEventListener('click', () => {
-    console.log(document.cookie);
-});
+    const cookieData = document.cookie.split(';');
+    cookieData.map(i => console.log(i.trim()));
+}); 
